7f2a049d586a06ad6f9255a56448a6f0169d73eb
petriwna <petriwna43@gmail.com>
2025-04-17 00:06:51 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
+
+<template>
+  <header>
+    <Header />
+  </header>
+  <main>
+  </main>
+  <footer></footer>
+</template>
