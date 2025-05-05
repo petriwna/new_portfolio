ab46079af4eb3a2c1cc8e0ca9c4362b93c10b6e3
petriwna <petriwna43@gmail.com>
2025-05-05 20:09:28 +0300

    add mixin for mediaquery

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,11 +1,27 @@
 <template>
   <div class="hero">
     <div class="hero__text">
-      <p class="hero__hi">Hi all. I am</p>
+      <div>
+        <p class="hero__hi">Hi all. I am</p>
+        <h1 class="hero__title">Nataliia Sychevska</h1>
+        <h2 class="hero__subtitle">
+          > Front-end developer
+        </h2>
+      </div>
+      <div>
+        <p class="hero__comments">// complete the game to continue</p>
+        <p class="hero__comments">// find my profile on Github:</p>
+        <p class="hero__code">const <span class="hero__code--var">githubLink</span> <span
+            class="hero__code--white">=</span> <a class="hero__code--link" href="https://github.com/petriwna">“https://github.com/petriwna”</a>
+        </p>
+      </div>
     </div>
     <div class="hero__game">
-
+      <div class="game">
+        <div class="game__display"></div>
+      </div>
     </div>
+    <div class="hero__bg"></div>
   </div>
 </template>
 <script setup lang="ts">
--- vite.config.ts
+++ vite.config.ts
@@ -1,46 +1,54 @@
-import { defineConfig, loadEnv } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import * as path from 'path'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd())
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd())
 
-  const port = Number(env.VITE_PORT) || 5173
+    const port = Number(env.VITE_PORT) || 5173
 
-  return {
-    plugins: [vue()],
+    return {
+        plugins: [vue()],
 
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src')
-      }
-    },
-
-    server: {
-      port,
-      open: true,
-      strictPort: true
-    },
-
-    build: {
-      outDir: 'dist',
-      assetsDir: '',
-      sourcemap: false,
-      rollupOptions: {
-        output: {
-          entryFileNames: 'js/[name].[hash].js',
-          chunkFileNames: 'js/[name].[hash].js',
-          assetFileNames: assetInfo => {
-            if (/\.(woff2?|ttf|otf|eot)$/.test(assetInfo.name ?? '')) {
-              return 'fonts/[name].[hash][extname]'
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src')
             }
-            if (/\.(png|jpe?g|gif|svg|webp)$/.test(assetInfo.name ?? '')) {
-              return 'images/[name].[hash][extname]'
+        },
+
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    includePaths: [path.resolve(__dirname, 'src/assets/styles')],
+                }
             }
-            return 'assets/[name].[hash][extname]'
-          },
+        },
+
+        server: {
+            port,
+            open: true,
+            strictPort: true
+        },
+
+        build: {
+            outDir: 'dist',
+            assetsDir: '',
+            sourcemap: false,
+            rollupOptions: {
+                output: {
+                    entryFileNames: 'js/[name].[hash].js',
+                    chunkFileNames: 'js/[name].[hash].js',
+                    assetFileNames: assetInfo => {
+                        if (/\.(woff2?|ttf|otf|eot)$/.test(assetInfo.name ?? '')) {
+                            return 'fonts/[name].[hash][extname]'
+                        }
+                        if (/\.(png|jpe?g|gif|svg|webp)$/.test(assetInfo.name ?? '')) {
+                            return 'images/[name].[hash][extname]'
+                        }
+                        return 'assets/[name].[hash][extname]'
+                    },
+                }
+            }
         }
-      }
     }
-  }
 })
